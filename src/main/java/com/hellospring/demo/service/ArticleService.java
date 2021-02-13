486260398c6f1e6f5538fe77906e1c44c9be54c5
fatossgorur <fatoss.98gorur@gmail.com>
2021-02-13 16:28:09 +0300

    Update ArticleService with getArticle

--- src/main/java/com/hellospring/demo/service/ArticleService.java
+++ src/main/java/com/hellospring/demo/service/ArticleService.java
@@ -4,6 +4,8 @@ import com.hellospring.demo.domain.Article;
 import com.hellospring.demo.repository.ArticleRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class ArticleService {
     private final ArticleRepository articleRepository;
@@ -19,5 +21,14 @@ public class ArticleService {
         } catch (Exception e) {
             return false;
         }
+    }
+
+    public Optional<Article> getArticle(String id){
+        try{
+            return articleRepository.findById(id);
+        }catch (Exception e){
+            return Optional.empty();
+        }
+
     }
 }
--- src/test/java/com/hellospring/demo/service/ArticleServiceTest.java
+++ src/test/java/com/hellospring/demo/service/ArticleServiceTest.java
@@ -9,6 +9,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Date;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
@@ -23,7 +26,7 @@ class ArticleServiceTest {
     ArticleRepository articleRepository;
 
     @Test
-    void it_should_create_article(){
+    void it_should_create_article() {
         //given
         var article = Mockito.mock(Article.class);
 
@@ -35,8 +38,9 @@ class ArticleServiceTest {
         assertThat(isCreated).isTrue();
 
     }
+
     @Test
-    void it_should_not_create_article_when_exception_occurred(){
+    void it_should_not_create_article_when_exception_occurred() {
         //given
         Article article = Mockito.mock(Article.class);
         given(articleRepository.save(article)).willThrow(RuntimeException.class);
@@ -49,4 +53,55 @@ class ArticleServiceTest {
 
     }
 
+    @Test
+    void it_should_get_article_with_id() {
+        //given
+        Article article = new Article();
+        article.setId("123");
+        article.setTitle("title");
+        article.setAuthorId("111");
+        article.setContent("content");
+        article.setCreateDate(new Date());
+        article.setUpdateDate(new Date());
+
+        given(articleRepository.findById("123")).willReturn(Optional.of(article));
+
+        //when
+        Optional<Article> optArticle = articleService.getArticle("123");
+
+        //then
+        assertThat(optArticle.isPresent()).isTrue();
+        Article originalArticle = optArticle.get();
+        assertThat(originalArticle.getId()).isEqualTo("123");
+        assertThat(originalArticle.getTitle()).isEqualTo("title");
+        assertThat(originalArticle.getAuthorId()).isEqualTo("111");
+        assertThat(originalArticle.getContent()).isEqualTo("content");
+        assertThat(originalArticle.getCreateDate()).isNotNull();
+        assertThat(originalArticle.getUpdateDate()).isNotNull();
+    }
+
+    @Test
+    void it_should_not_get_article_with_id() {
+        //given
+        given(articleRepository.findById("123")).willReturn(Optional.empty());
+
+        //when
+        Optional<Article> optArticle = articleService.getArticle("123");
+
+        //then
+        assertThat(optArticle.isPresent()).isFalse();
+
+    }
+
+    @Test
+    void it_should_not_get_article_when_exception_occurred() {
+        //given
+        given(articleRepository.findById("123")).willThrow(RuntimeException.class);
+
+        //when
+        Optional<Article> optArticle = articleService.getArticle("123");
+
+        //then
+        assertThat(optArticle.isPresent()).isFalse();
+    }
 }
